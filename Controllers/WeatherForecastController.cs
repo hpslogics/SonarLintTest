f3376298383b0d4cfcdb4d1118ae1a123a272574
Harpreet Singh <harsingh4@publicisgroupe.net>
2022-05-16 09:42:21 +0530

    commit changes

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -35,5 +35,30 @@ namespace SonarQubeTest.Controllers
             })
             .ToArray();
         }
+
+
+        [HttpGet]
+        [Route("GetDetails")]
+        public IEnumerable<WeatherForecast> GetDetails()
+        {
+            int calcValue = 123;
+
+            string objString = "TestString";
+
+
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)] + ":" + objString
+            })
+            .ToArray();
+
+
+        }
+
+
+
     }
 }
